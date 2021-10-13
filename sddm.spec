kdha200501/sddm

commit 022fc8f6bc3d6d688a5b0d38c3bfad2992123e21

Use systemd-sysusers config to setup user/group
See guidelines from https://docs.fedoraproject.org/en-US/packaging-guidelines/UsersAndGroups/

sddm.spec
@@ -64,6 +64,8 @@ Source15: README.scripts
 Source16: sddm.sysconfig
 # udev rules for disabling plasma-wayland in broken scenarios
 Source17: 61-sddm-plasmawayland.rules
+# systemd sysusers config
+Source18:  sddm-systemd-sysusers.conf
 
 Provides: service(graphical-login) = sddm
 
@@ -82,6 +84,7 @@ BuildRequires:  qt5-qttools-devel >= 5.6
 # verify presence to pull defaults from /etc/login.defs
 BuildRequires:  shadow-utils
 BuildRequires:  systemd
+BuildRequires:  systemd-rpm-macros
 
 Obsoletes: kde-settings-sddm < 20-5
 
@@ -153,6 +156,7 @@ install -Dpm 644 %{SOURCE13} %{buildroot}%{_tmpfilesdir}/sddm.conf
 install -Dpm 644 %{SOURCE14} %{buildroot}%{_sysconfdir}/sddm.conf
 install -Dpm 644 %{SOURCE15} %{buildroot}%{_datadir}/sddm/scripts/README.scripts
 install -Dpm 644 %{SOURCE16} %{buildroot}%{_sysconfdir}/sysconfig/sddm
+install -Dpm 644 %{SOURCE17} %{buildroot}%{_sysusersdir}/sddm.conf
 mkdir -p %{buildroot}/run/sddm
 mkdir -p %{buildroot}%{_localstatedir}/lib/sddm
 mkdir -p %{buildroot}%{_sysconfdir}/sddm/
@@ -168,11 +172,7 @@ install -Dpm 644 %{SOURCE17} %{buildroot}%{_udevrulesdir}/61-sddm-plasmawayland.
 touch %{buildroot}%{_sysconfdir}/sddm/hide-wayland-sessions
 
 %pre
-getent group sddm >/dev/null || groupadd -r sddm
-getent passwd sddm >/dev/null || \
-    useradd -r -g sddm -d %{_localstatedir}/lib/sddm -s /sbin/nologin \
-    -c "Simple Desktop Display Manager" sddm
-exit 0
+%sysusers_create_compat %{SOURCE18}
 
 %post
 %systemd_post sddm.service
@@ -236,6 +236,7 @@ fi
 %{_tmpfilesdir}/sddm.conf
 %{_udevrulesdir}/61-sddm-plasmawayland.rules
 %ghost %{_sysconfdir}/sddm/hide-wayland-sessions
+%{_sysusersdir}/sddm.conf
 %attr(0711, root, sddm) %dir /run/sddm
 %attr(1770, sddm, sddm) %dir %{_localstatedir}/lib/sddm
 %{_unitdir}/sddm.service

sddm-systemd-sysusers.conf
@@ -1,0 +1,1 @@
+u sddm - "Simple Desktop Display Manager" /var/lib/sddm

commit b0951104fba216e3c5bf6fd2b007829d37a8ee31

Use the correct source for sysusers config
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2009500
Fixes: 022fc8f Use systemd-sysusers config to setup user/group

sddm.spec
@@ -9,7 +9,7 @@
 
 Name:           sddm
 Version:        0.19.0
-Release:        15%{?dist}
+Release:        16%{?dist}
 License:        GPLv2+
 Summary:        QML based X11 desktop manager
 
@@ -156,7 +156,7 @@ install -Dpm 644 %{SOURCE13} %{buildroot}%{_tmpfilesdir}/sddm.conf
 install -Dpm 644 %{SOURCE14} %{buildroot}%{_sysconfdir}/sddm.conf
 install -Dpm 644 %{SOURCE15} %{buildroot}%{_datadir}/sddm/scripts/README.scripts
 install -Dpm 644 %{SOURCE16} %{buildroot}%{_sysconfdir}/sysconfig/sddm
-install -Dpm 644 %{SOURCE17} %{buildroot}%{_sysusersdir}/sddm.conf
+install -Dpm 644 %{SOURCE18} %{buildroot}%{_sysusersdir}/sddm.conf
 mkdir -p %{buildroot}/run/sddm
 mkdir -p %{buildroot}%{_localstatedir}/lib/sddm
 mkdir -p %{buildroot}%{_sysconfdir}/sddm/
@@ -260,6 +260,9 @@ fi
 
 
 %changelog
+* Wed Oct 13 2021 Timothée Ravier <tim@siosm.fr> - 0.19.0-16
+- Install the correct configuration for systemd-sysusers
+
 * Fri Jul 23 2021 Fedora Release Engineering <releng@fedoraproject.org> - 0.19.0-15
 - Rebuilt for https://fedoraproject.org/wiki/Fedora_35_Mass_Rebuild
 